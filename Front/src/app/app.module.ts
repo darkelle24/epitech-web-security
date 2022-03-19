591eab72f2db3c05fd0de5bdc2e0079b046f79e4
darkelle <djilani.cardineau@epitech.eu>
2022-03-19 15:41:35 +0100

    init component

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './body/login/login.component';
+import { RegisterComponent } from './body/register/register.component';
+import { FileListComponent } from './body/file-list/file-list.component';
+import { ProfileComponent } from './body/profile/profile.component';
+import { AddFileComponent } from './body/add-file/add-file.component';
+import { AdminPanelComponent } from './body/admin-panel/admin-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    FileListComponent,
+    ProfileComponent,
+    AddFileComponent,
+    AdminPanelComponent
   ],
   imports: [
     BrowserModule,
