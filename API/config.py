5b3cbe1abf9fb51d17bbc22ef6253951d7b8364b
antonin <antonin@alors-la.fr>
2022-03-26 07:01:05 +0100

    add : flask_cors package

--- API/config.py
+++ API/config.py
@@ -5,8 +5,10 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_mongoengine import MongoEngine
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 bcrypt = Bcrypt(app)
 
 MONGO_DB_COMMON_ALIAS = 'db_common'
